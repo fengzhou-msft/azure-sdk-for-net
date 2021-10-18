e1dca233489545febb91221b276d2079addbfed5
Feng Zhou <fenzhou@microsoft.com>
2021-10-18 09:46:29 +0800

    add tests for import

--- sdk/purview/Azure.Analytics.Purview.Catalog/tests/GlossaryClientTest.cs
+++ sdk/purview/Azure.Analytics.Purview.Catalog/tests/GlossaryClientTest.cs
@@ -29,6 +29,19 @@ namespace Azure.Analytics.Purview.Catalog.Tests
             JsonElement glossaryItemJson = fetchBodyJson[0];
             Assert.AreEqual("Glossary", glossaryItemJson.GetProperty("name").GetString());
         }
+        [RecordedTest]
+        public async Task ImportGlossaryTermsViaCsv()
+        {
+            var client = GetGlossariesClient();
+            var data = new JsonData(new Dictionary<string, string>
+            {
+                ["file"] = "\"Name\",\"Nick Name\",\"Status\",\"Definition\",\"Acronym\",\"Resources\",\"Related Terms\",\"Synonyms\",\"Stewards\",\"Experts\",\"Parent Term Name\"\n\"Parent Name 1_Parent Name 2_Term Name 1\",\"Term Name 1\",\"Draft\",\"Definition of Term Name 1\",\"TN1\",\"Purview Project:https://web.purview.azure.com;Azure portal:https://portal.azure.com;\",\"Term Name 4;Parent Term 2_Term Name 5;\",\"Term Name 2;Parent Name 1_Term Name 3;\",\"Feng.Zhou@microsoft.com\",\"Feng.Zhou@microsoft.com\",\"Parent Name 1_Parent Name 2\""
+            });
+            Operation<BinaryData> resultOperation = await client.ImportGlossaryTermsViaCsvAsync("6c57d99a-ddac-4f2a-bf8c-e55c5a7070d7", RequestContent.Create(data));
+            Response<BinaryData> fetchResponse = await resultOperation.WaitForCompletionAsync();
+            JsonElement fetchBodyJson = JsonDocument.Parse(fetchResponse.Value).RootElement;
+            Assert.AreEqual("RUNNING", fetchBodyJson.GetProperty("status").GetString());
+        }
         private static BinaryData GetContentFromResponse(Response r)
         {
             // Workaround azure/azure-sdk-for-net#21048, which prevents .Content from working when dealing with responses
